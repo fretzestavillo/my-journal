7169bbbf87cebbfbddb73ff94de1874b726de88c
fretzestavillo <estavillofretz@gmail.com>
2024-10-21 23:09:06 +0800

    change host name

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
+      host: 'localhost',
       port: 5432,
       username: 'journal',
       password: 'journal',
