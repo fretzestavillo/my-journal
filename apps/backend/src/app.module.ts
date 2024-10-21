90fc8afa902c4d69947444668bdb543ed1d0a61b
fretzestavillo <estavillofretz@gmail.com>
2024-10-21 22:54:04 +0800

    add type orm

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { JournalModule } from './app/journal.module';
+import { JournalEntity } from './app/journal.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [JournalModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'journal',
+      password: 'journal',
+      database: 'journal',
+      entities: [JournalEntity],
+      synchronize: true,
+    }),
+
+    JournalModule,
+  ],
 })
 export class AppModule {}
